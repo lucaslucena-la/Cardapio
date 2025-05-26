d2dd837b47596ad3c3c92ffe58ae4fded128fbee
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-05-25 22:38:53 -0300

    Rotas, Entidades, DTOs, Repositório

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -1,19 +1,103 @@
 package com.example.cardapio.food;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "foods")
 @Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;  //chave primária
+	
 	private String title;
+	
 	private String image;
+	
 	private Integer price;
+	
+	public Food(Long id, String title, String image, Integer price) {
+		this.id = id;
+		this.title = title;
+		this.image = image;
+		this.price = price;
+	}
+	
+	public Food(FoodRequestDTO data) {
+		this.image = data.image();
+		this.price = data.price();
+		this.title = data.title();
+	}
+	
+	public Food() {
+        // Construtor padrão
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, image, price, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Food other = (Food) obj;
+		return Objects.equals(id, other.id) && Objects.equals(image, other.image) && Objects.equals(price, other.price)
+				&& Objects.equals(title, other.title);
+	}
+
+	
+	
+	
 	
 }
