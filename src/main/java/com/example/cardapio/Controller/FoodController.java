5290b24766c1ea29d2422371a3bf31b2ed9f09a3
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-05-25 21:53:12 -0300

    Projeto Cardápio - inicial

---
+++ src/main/java/com/example/cardapio/Controller/FoodController.java
@@ -0,0 +1,27 @@
+package com.example.cardapio.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+	
+	@Autowired
+	private FoodRepository repository;
+	
+	@GetMapping
+	public List<Food> getAll() {
+		
+		
+		List<Food> foodList = repository.findAll();
+		return foodList;
+	}
+}
